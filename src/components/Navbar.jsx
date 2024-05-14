ccb9405850b03fb2e1903478c6840b2e7831eacd
ibrahimyryrn <ibrahimyeryaran@gmail.com>
2024-05-14 16:59:46 +0300

    ibo murat

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
       </div>
       <input className="search" type="text" placeholder="Search" />
       <p className="num-results">Found X results</p>
-      <p>trtrtrr</p>
+      <p>qwerty</p>
     </div>
   );
 }
